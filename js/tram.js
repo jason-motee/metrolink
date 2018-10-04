906dee947692ba1228f10fc420adf0e2d224a93a
jason.motee <jason.motee@autotrader.co.uk>
2018-10-04 15:25:10 +0100

    add Tram constructor and prototype methods

---
+++ js/tram.js
@@ -0,0 +1,22 @@
+function Tram (route, startingStation) {
+    this._route = route;
+    this._currentStation = startingStation;
+}
+
+Tram.prototype.getCurrentStation = function() {
+    return this._currentStation;
+}
+
+Tram.prototype.drive = function() {
+    var routeStations = this._route.getStations()
+    var currentStationIndex = routeStations.indexOf(this._currentStation)
+    var nextStationIndex = currentStationIndex + 1
+
+    if (nextStationIndex >= routeStations.length) {
+      return 'End of the line!'
+    }
+
+    this._currentStation = routeStations[nextStationIndex]
+
+    return 'Now arriving at: ' + this._currentStation.getName()
+}
